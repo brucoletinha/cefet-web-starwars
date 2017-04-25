97d1906857bb565c680e199b3d7f2aa721dbb8e3
unknown <brucoletinha@gmail.com>
2017-04-25 12:18:52 -0300

    Darth Vader is coming

--- starwars.js
+++ starwars.js
@@ -3,3 +3,21 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+let algarismoRomano = ['I','II','III','IV','V','IV','VII'];
+
+$.ajax({
+	url: 'http://swapi.co/api/films/',
+	type: 'GET',
+	success: function(result){
+		//$("#movies ul").html(result);
+		console.log(result);
+		$('#movies ul').html(null);
+		for(let filmes of result.results)
+		{
+			$('#movies ul').append($('<li>Episode ' + algarismoRomano[filmes.episode_id-1]+'</li>')
+        	.attr({ 'data-episode-url' : filmes.url}));
+		}
+	}
+});
+
